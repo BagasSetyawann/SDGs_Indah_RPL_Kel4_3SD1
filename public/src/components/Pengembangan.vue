6f192894bc7c1da686d47292da5492cdad99d80d
bagaz6645 <222111947@stis.ac.id>
2023-12-22 21:25:07 +0700

    Code program fix

--- public/src/components/Pengembangan.vue
+++ public/src/components/Pengembangan.vue
@@ -15,47 +15,6 @@
         </div>
         <!-- Spinner End -->
   
-        <!-- Navbar Start -->
-        <nav class="navbar navbar-expand-lg bg-white navbar-light shadow sticky-top p-0">
-            <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarCollapse">
-                <div class="navbar-nav ms-auto p-4 p-lg-0">
-                    <router-link to="/" class="nav-item nav-link">Menu Utama</router-link>               
-                </div>
-            </div>
-            <div class="collapse navbar-collapse" id="navbarCollapse">
-            <router-link to="/login" class="btn btn-success text-light rounded-pill py-2 px-4 ms-3">Masuk</router-link>
-            </div>
-            <!-- header start -->
-            <header>
-              <div class="nav-icons" onclick="toggleMenu()">
-                  <i class='bx bx-user'></i>
-              
-              </div>
-            </header>
-            <div class="sub-menu-wrap" id="subMenu" >
-                <div class="sub-menu">
-                    <div class="user-info text-center">
-                        <i class='bx bxs-user-circle fa-6x'></i>
-                        <h3>Vladimir Putin</h3>
-                        <h5>Bappenas</h5>
-                    </div>
-                    <hr>
-
-                    
-                    <router-link to="" class="sub-menu-link">
-                        <i class='bx bx-log-out fa-2x'></i>
-                        <p>Logout</p>
-                    </router-link>
-                
-                </div>
-            </div>
-            <!-- header end -->
-        </nav>
-        <!-- Navbar End -->
-  
         <!-- Form Start -->
         <div class="container-xxl py-5 wow fadeInUp" data-wow-delay="0.1s">
           <div class="container">
@@ -73,37 +32,6 @@
           </div>
       </div>
       <!-- Form End -->
-  
-  
-        <!-- Footer Start -->
-        <div
-          class="container-fluid bg-dark text-white-50 footer pt-5 mt-5 wow fadeIn"
-          data-wow-delay="0.1s"
-        >
-          <div class="container py-5">
-            <div class="row g-5">
-              <div class="col-lg-3 col-md-6">
-                <h5 class="text-white mb-4">Layanan Disediakan Oleh:</h5>
-                <a href="bps.go.id"
-                  ><img src="img/bps.png" alt="BPS" style="width: 250px"
-                /></a>
-              </div>
-            </div>
-          </div>
-          <div class="container">
-            <div class="copyright">
-              <div class="row">
-                <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                  &copy;
-                  <a class="border-bottom" href="bps.go.id"
-                    >Badan Pusat Statistik</a
-                  >, All Right Reserved.
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <!-- Footer End -->
   
         <!-- Back to Top -->
         <a href="#" class="btn btn-lg btn-success btn-lg-square back-to-top"
